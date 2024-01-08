8e74fcb797d643dee5a2829b208569e75adbd939
kulikovjob <kulikovvadimjob@gmail.com>
2024-01-08 09:10:36 +0200

    added slider

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="slick-prev"/></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="slick-next"/></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
